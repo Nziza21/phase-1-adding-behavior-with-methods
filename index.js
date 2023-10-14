bf3baf48c1ab9d962050447e3e1ed4e234a78a94
Nziza21 <144722415+Nziza21@users.noreply.github.com>
2023-10-14 17:34:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    return `${this.name} says meow!`;
+  }
+}
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    return `${this.name} says woof!`;
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    if (this.sex === "male") {
+      return `It's me! ${this.name}, the parrot!`;
+    } else {
+      return `${this.name} says squawk!`;
+    }
+  }
+}
+const cat = new Cat("Whiskers", "female");
+const dog = new Dog("Rover", "male");
+const bird = new Bird("Polly", "male");
+
+console.log(cat.speak()); // Whiskers says meow!
+console.log(dog.speak()); // Rover says woof!
+console.log(bird.speak()); // It's me! Polly, the parrot!
